d17f6d5558ca4687b9e328f700b7e33ec9e23ddf
padmashrimallapur <padmashri.mallapur@gmail.com>
2016-05-12 16:45:27 -0400

    Following changes has been made 1) Login Page has been added 2) Validation has been added for all the fields present int the login page.

--- blog/client/main.js
+++ blog/client/main.js
@@ -40,8 +40,24 @@ if (Meteor.isClient) {
       Router.go('login');
     }
   });
-
 }
+
+Template.login.events({
+  'submit form': function(event){
+    event.preventDefault();
+    var username = $('[name = username]').val();
+    var password = $('[name = password]').val();
+    Meteor.loginWithPassword(username, password, function(error){
+      if(error){
+        console.log(error.reason);
+      }
+      else{
+        console.log("you are successfully logged in");
+        Router.go('home');
+      }
+    });
+  }
+});
 
 Meteor.methods({
   checkPassword: function(password, confirmPassword){

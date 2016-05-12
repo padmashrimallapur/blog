183878409cd4dcdfd0e4e06822e4b10cbc7b3b8b
padmashrimallapur <padmashri.mallapur@gmail.com>
2016-05-12 15:57:45 -0400

    Following changes has been made 1) Email -Id and Confirm password field has been added to registration page 2) Validation has been added for all the fields present int the registration page.

--- blog/client/main.js
+++ blog/client/main.js
@@ -4,15 +4,33 @@ if (Meteor.isClient) {
   Template.register.events ({
     'submit form' : function(event){
       event.preventDefault();
+      var email  = $('[name= email]').val();
       var username = $('[name=username]').val();
       var password = $('[name=password]').val();
+      var confirmPassword = $('[name= confirmPassword]').val();
+      Meteor.call('checkPassword', password, confirmPassword, function(err, result){
+        if(result){
+          console.log("The password matches");
+        }
+        else{
+          console.log("Email has been already registered with the other user");
+        }
+      });
       Accounts.createUser({
         username : username,
-        password : password
-      });
-
-      Router.go('home');
-    }
+        password : password,
+        email : email,
+        confirmPassword : confirmPassword},
+          function (error) {
+            if(error){
+            //error info
+              return false;
+          }else {
+            //
+              Router.go('home');
+            }
+          });
+      }
   });
 
   Template.navigation.events({
@@ -25,6 +43,15 @@ if (Meteor.isClient) {
 
 }
 
+Meteor.methods({
+  checkPassword: function(password, confirmPassword){
+   // check(password, String);
+   // check(confirmPassword, String);
+    if(password != confirmPassword){
+      console.log("Password should match");
+    }
+  }
+})
 
 Router.route('/register');
 Router.route('/login');

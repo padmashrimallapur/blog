f2941eaec44a605093e98ad6febd7fa10d0fb0c5
padmashrimallapur <padmashri.mallapur@gmail.com>
2016-05-25 23:01:29 -0400

    Following changes have been done 1) Edit and delete functionality are working. 2) The user without login can see all the blogs that are published by other authors and as well as by him. 3) On click of the blog title user will be taken to different window to show how the perticular article, which will tell who is the author of the article and when it has been published. 4) Edit and delete of the blog is working.

--- blog/client/main.js
+++ blog/client/main.js
@@ -2,123 +2,152 @@ Blogs = new Mongo.Collection('Blogs', {idGeneration: 'MONGO'});
 
 if (Meteor.isClient) {
 
-  Template.registerHelper('avatar',function(input){
-      return  "/images/default-user.png";
-  });
+    Template.registerHelper('avatar', function (input) {
+        return "/images/default-user.png";
+    });
 
-  Router.route('/register');
-  Template.register.events ({
-    'submit form' : function(event){
-      event.preventDefault();
-      var email  = $('[name= email]').val();
-      var username = $('[name=username]').val();
-      var password = $('[name=password]').val();
-      var confirmPassword = $('[name= confirmPassword]').val();
-      Meteor.call('checkPassword', password, confirmPassword, function(err, result){
-        if(result){
-          console.log("The password matches");
+    Router.route('/register');
+    Template.register.events({
+        'submit form': function (event) {
+            event.preventDefault();
+            var email = $('[name= email]').val();
+            var username = $('[name=username]').val();
+            var password = $('[name=password]').val();
+            var confirmPassword = $('[name= confirmPassword]').val();
+            Meteor.call('checkPassword', password, confirmPassword, function (err, result) {
+                if (result) {
+                    console.log("The password matches");
+                }
+                else {
+                    console.log("Email has been already registered with the other user");
+                }
+            });
+            Accounts.createUser({
+                    username: username,
+                    password: password,
+                    email: email,
+                    confirmPassword: confirmPassword
+                },
+                function (error) {
+                    if (error) {
+                        return false;
+                    } else {
+                        Router.go('home');
+                    }
+                });
         }
-        else{
-          console.log("Email has been already registered with the other user");
-        }
-      });
-      Accounts.createUser({
-        username : username,
-        password : password,
-        email : email,
-        confirmPassword : confirmPassword},
-          function (error) {
-            if(error){
-              return false;
-          } else {
-              Router.go('home');
-            }
-          });
-      }
-  });
+    });
 
-  Template.navigation.events({
-    'click .logout': function(event){
-      event.preventDefault();
-      Meteor.logout();
-      Router.go('home');
-    }
-  });
+    Template.navigation.events({
+        'click .logout': function (event) {
+            event.preventDefault();
+            Meteor.logout();
+            Router.go('home');
+        }
+    });
 }
 
 Router.route('/login');
 Template.login.events({
-  'submit form': function(event){
-    event.preventDefault();
-    var username = $('[name = username]').val();
-    var password = $('[name = password]').val();
-    Meteor.loginWithPassword(username, password, function(error){
-      if(error){
-        console.log(error.reason);
-      }
-      else{
-          console.log("you are successfully logged in");
-          Router.go('home');
-      }
-    });
-  }
+    'submit form': function (event) {
+        event.preventDefault();
+        var username = $('[name = username]').val();
+        var password = $('[name = password]').val();
+        Meteor.loginWithPassword(username, password, function (error) {
+            if (error) {
+                console.log(error.reason);
+            }
+            else {
+                console.log("you are successfully logged in");
+                Router.go('home');
+            }
+        });
+    }
 });
 
 Meteor.methods({
-  checkPassword: function(password, confirmPassword){
-   // check(password, String);
-   // check(confirmPassword, String);
-    if(password != confirmPassword){
-      console.log("Password should match");
+    checkPassword: function (password, confirmPassword) {
+        if (password != confirmPassword) {
+            console.log("Password should match");
+        }
     }
-  }
 });
 
 Router.route('/newblog');
 Template.newblog.events({
-  'submit form': function (event) {
-    var firstName = $('[name=bloggerName]').val();
-    var bloggerLastName = $('[name=bloggerLastName]').val();
-    var article = $('[name=article]').val();
-    var title = $('[name=title]').val();
-    var username = Meteor.user().username;
-    Meteor.call("submitPost", firstName, title, bloggerLastName, article, username);
-  }
+    'submit form': function (event) {
+        var firstName = $('[name=bloggerName]').val();
+        var bloggerLastName = $('[name=bloggerLastName]').val();
+        var article = $('[name=article]').val();
+        var title = $('[name=title]').val();
+        var username = Meteor.user().username;
+        Meteor.call("submitPost", firstName, title, bloggerLastName, article, username);
+    }
 });
 
 Router.route('/listblogs');
-Template.listblogs.myblogs = function(){
-  username = Meteor.user().username;
-  return Blogs.find({"username" : username});
+Template.listblogs.myblogs = function () {
+    username = Meteor.user().username;
+    return Blogs.find({"username": username});
 };
 
 Router.route('/editBlog');
+Router.route('/newWindow');
+
 Template.listblogs.events({
-  'click button' : function (event) {
-    var blogId = event.target.value;
+    'click .edit': function (event) {
+        var blogId = event.target.value;
 
-    Template.editBlog.currentBlog= function () {
-      return Blogs.find({"_id" : blogId});
+        Template.editBlog.currentBlog = function () {
+            return Blogs.find({"_id": blogId});
+        };
+        Router.go('editBlog');
+    },
+    "click .deleteBlog": function (event) {
+        Blogs.remove(this._id);
+    }
 
-    };
-    Router.go('editBlog');
-  }
+});
+
+Router.route('/home');
+Template.home.allblogs = function () {
+    return Blogs.find();
+};
+
+Router.route('/viewBlog/:_id', function () {
+    this.render("viewOneBlog", {
+        data: function () {
+            console.log(this.params._id);
+            console.log(Blogs.findOne({_id: this.params._id}));
+            return Blogs.findOne({_id: this.params._id});
+        }
+    });
 });
 
 
-Router.route('/home');
-Template.home.allblogs = function(){
-  return Blogs.find();
-};
+Template.editBlog.events({
+    "submit form": function (events) {
+        var firstName = $('[name=bloggerName]').val();
+        var bloggerLastName = $('[name=bloggerLastName]').val();
+        var article = $('[name=article]').val();
+        var title = $('[name=title]').val();
+        var blogId = $('[name=blogId]').val();
+        var username = Meteor.user().username;
 
-Router.route('/viewBlog');
-Template.home.events({
-  'click href' : function (event) {
-    var blogId = event.target.value;
+        //Posts.update('theCopied_Id', {$set: {title: 'Wow the title changed!'}});
+        Meteor.call("updateBlog", firstName, title, bloggerLastName, article, username, blogId)
 
-    Template.editBlog.currentBlog= function () {
-      return Blogs.find({"_id" : blogId});
-    };
-    Router.go('viewBlog');
-  }
+    }
+});
+
+// helper to get the date format
+Template.registerHelper("dateFormater", function (timestamp) {
+    console.log(moment(timestamp).format("MM.DD.YYYY"));
+    return moment(timestamp).format("MM.DD.YYYY");
+
+});
+
+
+Template.registerHelper("isEmpty", function (object) {
+    return jQuery.isEmpty(object);
 });
--- blog/server/main.js
+++ blog/server/main.js
@@ -13,6 +13,33 @@ Meteor.methods({
                 date: new Date().toString()
             })
         }
+    },
+
+    'updateBlog' : function (firstname, title, bloggerLastname, article, username, blogId) {
+        if(Meteor.user().username === username){
+            debugger;
+            Blogs.update( blogId, {$set: {
+
+                title: title,
+                firstname: firstname,
+                bloggerLastname: bloggerLastname,
+                article: article,
+                username: username,
+                date: new Date().toString(), function(error){
+
+                    if (error) {
+                        alert("vnkvnkv");
+                        console.log(error);
+                    } else {
+
+                        alert("ascosajlca");
+                        console.log("askckajhkahvk");
+                    }
+
+                }
+            }
+            })
+        }
     }
     
 });
